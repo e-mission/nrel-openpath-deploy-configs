d119b5e9cfa9a89a2a513a678311355854f69036
Natalie Schultz <90212258+nataliejschultz@users.noreply.github.com>
2023-11-28 14:12:47 -0700

    Addressing requested changes
    Push addresses the clean-up changes requested.
    
    Adds a main function for code that should not be run outside of the file.
    
    Adds authentication for sts client.
    
    Simplifies get_userpool_name and user_already_exists functions
    
    Updates format_email function to add further customization of welcome email based on admindash_prefs section of config

--- email_automation/email-config.py
+++ email_automation/email-config.py
@@ -6,14 +6,16 @@ import logging
 import sys
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+# If you don't have boto3 installed, make sure to `pip install boto3` before running this script. 
 
 #Input the filename as an argument in command line 
-filename_raw = sys.argv[1]
-filename = filename_raw.split(".")[0]
-pool_name = "nrelopenpath-prod-" + filename
-
-current_path = os.path.dirname(__file__)
-config_path = os.path.relpath('../configs/'+ filename_raw, current_path)
+if __name__ == "__main__":
+    filename_raw = sys.argv[1]
+    filename = filename_raw.split(".")[0]
+    pool_name = "nrelopenpath-prod-" + filename
+    # pool_name = 'nrelopenpath-stage'
+    current_path = os.path.dirname(__file__)
+    config_path = os.path.relpath('../configs/'+ filename_raw, current_path)
 
 #Set up AWS credentials as environment variables + set variables 
 ACCESS = os.environ.get("AWS_ACCESS_KEY_ID")
@@ -29,47 +31,32 @@ cognito_client = boto3.client(
     aws_session_token=TOKEN, 
     region_name=AWS_REGION
     )
-sts_client = boto3.client("sts")
-
-
+sts_client = boto3.client(
+    'sts',
+    aws_access_key_id = ACCESS,
+    aws_secret_access_key= SECRET,
+    aws_session_token=TOKEN, 
+    region_name=AWS_REGION
+    )
+# Functions 
 def get_userpool_name(pool_name, cognito_client):
     response = cognito_client.list_user_pools(MaxResults=60)
-    UserPoolExist = False
-    #########One option to set the user pool without breaking (but still stop when condition met)
-    i = 0
-    while response["UserPools"][i]["Name"] != pool_name and i < len(response["UserPools"]) - 1:
-        print("looking for user pool...")
-        i = i + 1
-        if response["UserPools"][i]["Name"] == pool_name:
-            UserPoolExist = True
-            pool_id = response["UserPools"][i]["Id"]
-            print(pool_name + " pool exists! Checking for users...")
-    #########Second option that uses a break when condition is met:
-    # for i in response["UserPools"]:
-    #     if i["Name"] == pool_name and not UserPoolExist:
-    #         pool_id = i["Id"]
-    #         UserPoolExist = True
-    #         print(pool_name + " pool exists! Checking for users...")
-    #         break 
-    #     else:
-    #         print("Looking for pool...")
-    #         continue
-
-    return UserPoolExist, pool_id
+    is_userpool_exist = False    
+    user_pools = [user_pool["Name"] for user_pool in response["UserPools"]]
+    is_userpool_exist = True if pool_name in user_pools else False
+    user_pool_index = user_pools.index(pool_name) if is_userpool_exist else None
+    pool_id = response["UserPools"][user_pool_index]["Id"]
+    return is_userpool_exist, pool_id
 
 def user_already_exists(pool_id, email, cognito_client):
+
     try:
         response = cognito_client.list_users(UserPoolId=pool_id)
         users = response["Users"]
+        print('users:' , users)
         result = False
-        for i in users:
-            for k, v in i.items():
-                if k == "Attributes":
-                    for j in v:
-                        if j["Name"] == "email":
-                            user_email = str(j["Value"])
-                            if str(email) == user_email:
-                                result = True
+        if str(users).find(email) > 1:
+            result = True
         return result
     except ClientError as err:
         logger.error(
@@ -90,11 +77,11 @@ def email_extract():
         data = json.load(config_file)
         intro = data['intro']
         emails = [i.strip() for i in intro['admin_users'].split(",")]
-        ####This section is in preparation for the next-level goal, which may involve customizing the email further based on the submission form.
-        # admindash_prefs = data['admin_dashboard']
-        # columns_exclude = admin_dash['data_trajectories_columns_exclude']
-        # print("admindash", admindash_prefs)
-    return emails
+        admindash_prefs = data['admin_dashboard']
+        #maybe set a bool and if this is true, then add a section to the HTML. Otherwise leave it. Need to do the same for map trip lines tomorrow.
+        columns_exclude = admindash_prefs['data_trips_columns_exclude']
+        map_trip_lines_enabled = admindash_prefs['map_trip_lines']
+    return emails, map_trip_lines_enabled, columns_exclude
 
 def create_account(pool_id, email, cognito_client):
     response = cognito_client.admin_create_user(
@@ -112,11 +99,21 @@ def create_account(pool_id, email, cognito_client):
                     ],
                 )
     return response
-def format_email(pool_name):
-    with open("welcome-template.txt", "r") as f:
+
+def format_email(pool_name, map_trip_lines_enabled, columns_exclude):
+    with open('welcome-template.txt', 'r') as f:
         html = f.read()
-        html = html.replace("<pool_name>", pool_name)
-    return html
+        html = html.replace('<pool_name>', pool_name)
+        if map_trip_lines_enabled:
+            html = html.replace ('<map_trip_lines>', 'Additionally, you can view individual user-origin destination points.')
+        else:
+            html = html.replace ('<map_trip_lines>', '')
+        if 'data.start_loc.coordinates' in columns_exclude or 'data.end_loc.coordinates' in columns_exclude:
+            html = html.replace ('<columns_exclude>', 'Your configuration excludes trip start/end in the trip table. Let us know if you would like to include those.')
+        elif columns_exclude == '':
+            html = html.replace ('<columns_exclude>', 'Since you indicated that you want to map the data to infrastructure updates, your configuration includes trip start/end in the trip table. Let us know if you would like to exclude those for greater anonymity.')
+        return html
+        
 
 def update_user_pool(pool_id, pool_name, html, identity_arn, cognito_client):
   response = cognito_client.update_user_pool(
@@ -141,21 +138,19 @@ def update_user_pool(pool_id, pool_name, html, identity_arn, cognito_client):
             }
         },
 )
-  
 ######################################################################
-
-UserPoolExist, pool_id = get_userpool_name(pool_name, cognito_client) 
+is_userpool_exist, pool_id = get_userpool_name(pool_name, cognito_client) 
 
  # Start by checking for the User Pool. If the User Pool does not yet exist, wait until it is set up to add users. 
-if UserPoolExist:
+if is_userpool_exist:
     #extract email addresses from conf file
-    emails = email_extract()
+    emails, map_trip_lines_enabled, columns_exclude = email_extract()
     #Loop over each email address. Check if they're in the user pool.
     for email in emails:
         if not user_already_exists(pool_id, email, cognito_client):   
             #If user not in pool, format the email template for their welcome email, update the user pool, and create an account for them.
             print(email + " not in user pool! Creating account...")
-            html = format_email(pool_name)
+            html = format_email(pool_name, map_trip_lines_enabled, columns_exclude)
             identity_arn = get_verified_arn(sts_client)
             update_user_pool(pool_id, pool_name, html, identity_arn, cognito_client)
             response = create_account(pool_id, email, cognito_client)
