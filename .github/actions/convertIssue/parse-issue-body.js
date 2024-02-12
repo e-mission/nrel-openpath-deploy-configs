eeb6686c4080eb4be51d0e1df82138b7cf810749
Sebastian Barry <sebbarry@comcast.net>
2024-02-12 10:10:42 -0700

    Update text

--- .github/actions/convertIssue/parse-issue-body.js
+++ .github/actions/convertIssue/parse-issue-body.js
@@ -278,7 +278,7 @@ export async function parseIssueBody(githubIssueTemplateFile, body) {
     let connect_url =
       "https://" + combinedObject.url_abbreviation + "-openpath.nrel.gov/api/";
     configObject["server"] = {
-      connectURL: connect_url,
+      connectUrl: connect_url,
       aggregate_call_auth: "user_only",
     }; //TODO check options for call + add to form?
 
